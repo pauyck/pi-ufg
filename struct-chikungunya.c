a4ad6ac5d47d0d274d3d6867e383210e3df65294
Paulo Henrique de Abreu Neiva <pauyck@gmail.com>
2017-05-12 20:18:17 -0300

    Add files via upload

---
+++ struct-chikungunya.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+voi preencherDados();
+
+int main () {
+    struct Endereco{
+       string logradouro[64];
+       string bairro[64];
+       string cidade[32];
+       string estado[2];
+        };
+
+    typedef struct {
+        string nome[25];
+        string telefone[12];
+        Endereco endereco[50];
+        string genero[10];
+        string gestante[3];
+        int contaminacao;
+    }pessoa;
+
+
+
+
+0 => Não contaminado;
+1 => Contaminado com Dengue;
+2 => Contaminado com Zika Vírus;
+3 => Contaminado com Chikungunya.
+
+
+}
